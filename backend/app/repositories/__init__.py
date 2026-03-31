98afac9222c83d1fe3cdb48e600a533690c93706
Mueed Ahmad <mueedahmad067@gmail.com>
2026-04-01 00:00:14 +0500

    feat: added the UserRepository

---
+++ backend/app/repositories/__init__.py
@@ -0,0 +1,6 @@
+from .user import UserRepository, get_user_repository
+
+__all__ = [
+    "UserRepository",
+    "get_user_repository",
+]
---
+++ backend/app/repositories/user.py
@@ -0,0 +1,31 @@
+from sqlalchemy import or_, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models import DBUser
+from core.dependencies import Session_Dep
+from core.repository import BaseRepository
+
+
+class UserRepository(BaseRepository[DBUser]):
+    def __init__(
+        self,
+        session: AsyncSession,
+    ) -> None:
+        super().__init__(DBUser, session)
+
+    async def get_by_username(self, username: str) -> DBUser | None:
+        return await self.get_one_by_filters({"username": username})
+
+    async def get_by_email(self, email: str) -> DBUser | None:
+        return await self.get_one_by_filters({"email": email})
+
+    async def get_by_username_or_email(self, value: str) -> DBUser | None:
+        stmt = select(DBUser).where(
+            or_(DBUser.username == value, DBUser.email == value)
+        )
+        result = await self.session.execute(stmt)
+        return result.scalars().first()
+
+
+def get_user_repository(session: Session_Dep) -> UserRepository:
+    return UserRepository(session)
