7d8b366c2b4d6bd692adde667abad49fa19c1000
Mueed Ahmad <mueedahmad067@gmail.com>
2026-03-31 21:09:11 +0500

    feat: added the dbbase

--- backend/core/database/__init__.py
+++ backend/core/database/__init__.py
@@ -1,3 +1,4 @@
+from .base import DBBase
 from .session import get_session
 
-__all__ = ["get_session"]
+__all__ = ["get_session", "DBBase"]
---
+++ backend/core/database/base.py
@@ -0,0 +1,6 @@
+
+from sqlalchemy.orm import DeclarativeBase
+
+
+class DBBase(DeclarativeBase):
+    pass
