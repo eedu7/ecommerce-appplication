1db0969f64672eb1a3429bb2e00a29230e1a58ba
eedu7 <mueedahmad067@gmail.com>
2026-04-01 20:10:33 +0500

    feat: added the form context and hooks

---
+++ frontend/src/hooks/use-app-form.ts
@@ -0,0 +1,12 @@
+import { createFormHook, createFormHookContexts } from "@tanstack/react-form"
+
+const { fieldContext, formContext } = createFormHookContexts();
+
+export const { useAppForm } = createFormHook({
+  fieldComponents: {
+  },
+  formComponents: {
+  },
+  fieldContext,
+  formContext,
+});
