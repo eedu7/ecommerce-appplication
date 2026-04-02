fe06abc7dbb79427ade7825327acf913297b65d4
eedu7 <mueedahmad067@gmail.com>
2026-04-02 13:19:49 +0500

    feat: added the provider

--- frontend/src/providers/tanstack-query/tanstack-query-client-provider.tsx
+++ frontend/src/providers/tanstack-query/tanstack-query-client-provider.tsx
@@ -1,18 +1,10 @@
-"use client"
+"use client";
 
-import { QueryClientProvider } from "@tanstack/react-query"
-import { getQueryClient } from "./get-query-client"
-import { ReactNode } from "react"
+import { QueryClientProvider } from "@tanstack/react-query";
+import { getQueryClient } from "./get-query-client";
+import { ReactNode } from "react";
 
-export const TanstackQueryClientProvider = ({
-    children,
-}: {
-    children: ReactNode
-}) => {
-    const queryClient = getQueryClient()
-    return (
-        <QueryClientProvider client={queryClient}>
-            {children}
-        </QueryClientProvider>
-    )
-}
+export const TanstackQueryClientProvider = ({ children }: { children: ReactNode }) => {
+    const queryClient = getQueryClient();
+    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+};
