9ae28248dab2b8c774155cd1e6f374223102cd6d
eedu7 <mueedahmad067@gmail.com>
2026-04-02 10:07:36 +0500

    feat: added the register form

--- frontend/src/features/auth/components/register-form.tsx
+++ frontend/src/features/auth/components/register-form.tsx
@@ -1,5 +1,85 @@
-"use client"
+"use client";
+
+import { useAppForm } from "@/hooks/use-app-form";
+import { revalidateLogic } from "@tanstack/form-core";
+import { registerUserSchema } from "@/features/auth/auth.schema";
+import { FieldGroup, FieldSet } from "@/components/ui/field";
 
 export const RegisterForm = () => {
-    return <form></form>
-}
+    const form = useAppForm({
+        defaultValues: {
+            username: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+        },
+        validationLogic: revalidateLogic(),
+        validators: {
+            onDynamic: registerUserSchema,
+        },
+        onSubmit: async ({ value }) => {
+            console.table(value);
+        },
+    });
+
+    return (
+        <form
+            onSubmit={async (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                await form.handleSubmit();
+            }}
+        >
+            <FieldGroup>
+                <FieldSet>
+                    <FieldGroup>
+                        <form.AppField
+                            name="username"
+                            children={(field) => (
+                                <field.TextField
+                                    label="Username"
+                                    name="username"
+                                    autoComplete="username"
+                                    autoCapitalize="none"
+                                    spellCheck="false"
+                                />
+                            )}
+                        />
+                        <form.AppField
+                            name="email"
+                            children={(field) => (
+                                <field.TextField
+                                    label="Email"
+                                    name="email"
+                                    autoComplete="email"
+                                    autoCapitalize="none"
+                                    spellCheck="false"
+                                    type="email"
+                                />
+                            )}
+                        />
+                        <form.AppField
+                            name="password"
+                            children={(field) => (
+                                <field.PasswordField label="Password" name="password" autoComplete="new-password" />
+                            )}
+                        />
+                        <form.AppField
+                            name="confirmPassword"
+                            children={(field) => (
+                                <field.PasswordField
+                                    label="Confirm Password"
+                                    name="confirmPassword"
+                                    autoComplete="new-password"
+                                />
+                            )}
+                        />
+                    </FieldGroup>
+                </FieldSet>
+                <form.AppForm>
+                    <form.SubmitButton label="Register" isPending={false} />
+                </form.AppForm>
+            </FieldGroup>
+        </form>
+    );
+};
