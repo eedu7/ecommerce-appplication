58b933c77a10ab0b6c48707614c6282dafc12d07
eedu7 <mueedahmad067@gmail.com>
2026-04-02 18:36:55 +0500

    updated the UserOut, added the uid and role

--- backend/app/schemas/responses/user.py
+++ backend/app/schemas/responses/user.py
@@ -1,8 +1,14 @@
+from uuid import UUID
+
 from pydantic import BaseModel, ConfigDict
+
+from app.models import DBUserRole
 
 
 class UserOut(BaseModel):
+    uid: UUID
     username: str
     email: str
+    role: DBUserRole
 
     model_config = ConfigDict(from_attributes=True)
