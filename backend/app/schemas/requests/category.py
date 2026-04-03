19baf639fe007985b7033412f08e27fbc3ac2bf4
eedu7 <mueedahmad067@gmail.com>
2026-04-03 11:57:46 +0500

    feat: updated thte category schema

--- backend/app/schemas/requests/category.py
+++ backend/app/schemas/requests/category.py
@@ -1,16 +1,21 @@
+from uuid import UUID
+
 from pydantic import BaseModel, Field
 
 
 class CategoryIn(BaseModel):
     name: str = Field(..., examples=["Clothes"])
     description: str | None = Field(None, examples=["Item you wear"])
+    parent_id: UUID | None = None
 
 
 class CategoryUpdate(BaseModel):
     name: str
-    description: str | None = None
+    description: str
+    parent_id: UUID
 
 
 class CategoryPartialUpdate(CategoryIn):
     name: str | None = None
     description: str | None = None
+    parent_id: UUID | None = None
--- backend/app/schemas/responses/category.py
+++ backend/app/schemas/responses/category.py
@@ -1,3 +1,4 @@
+from typing import List
 from uuid import UUID
 
 from pydantic import BaseModel, ConfigDict
@@ -7,4 +8,10 @@ class CategoryOut(BaseModel):
     uuid: UUID
     name: str
     description: str
+    parent_id: UUID | None = None
+    children: List["CategoryOut"] = []
+
     model_config = ConfigDict(from_attributes=True)
+
+
+CategoryOut.model_rebuild()
