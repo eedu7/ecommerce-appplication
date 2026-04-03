4a59bf400d6c3df80f8910a1e80979b8e475a6a3
eedu7 <mueedahmad067@gmail.com>
2026-04-03 11:50:56 +0500

    Feat: updated the DBCategory

--- backend/app/models/category.py
+++ backend/app/models/category.py
@@ -1,5 +1,8 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import List
+from uuid import UUID
+
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.database import DBBase
 from core.database.mixins import PrimaryKeyMixin, TimestampMixin
@@ -15,6 +18,25 @@ class DBCategory(DBBase, PrimaryKeyMixin, TimestampMixin):
     description: Mapped[str | None] = mapped_column(
         String(256),
         nullable=True,
+    )
+
+    # Relationships
+    parent_id: Mapped[UUID | None] = mapped_column(
+        ForeignKey(
+            "categories.uid",
+            onupdate="SET NULL",
+        ),
+        nullable=True,
+    )
+    parent: Mapped["DBCategory" | None] = relationship(
+        "DBCategory",
+        remote_side="DBCategory.uid",
+        back_populates="children",
+    )
+    children: Mapped[List["DBCategory"]] = relationship(
+        "DBCategory",
+        back_populates="parent",
+        cascade="all, delete-orphan",
     )
 
     def __repr__(self) -> str:
