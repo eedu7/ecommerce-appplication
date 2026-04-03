093a8cdbd01acb0c45e33d1a13ebae995464b48f
eedu7 <mueedahmad067@gmail.com>
2026-04-03 15:35:00 +0500

    feat: updated the category page

--- frontend/src/features/category/pages/category-page.tsx
+++ frontend/src/features/category/pages/category-page.tsx
@@ -1,10 +1,9 @@
 import { CategoryDataTable } from "@/features/category/components/category-data-table";
-import { categoryColumns } from "@/features/category/components/category-columns";
 
 export const CategoryPage = () => {
     return (
         <main>
-            <CategoryDataTable columns={categoryColumns} data={[]} />
+            <CategoryDataTable />
         </main>
     );
 };
