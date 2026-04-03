c8ee710cff23ad256665dc91352d44cf336f14f9
eedu7 <mueedahmad067@gmail.com>
2026-04-03 11:14:03 +0500

    feat: addded the get_by_name method and create category method

--- backend/app/controllers/category.py
+++ backend/app/controllers/category.py
@@ -1,6 +1,8 @@
 from app.models import DBCategory
 from app.repositories.category import CategoryRepository
+from app.schemas.requests.category import CategoryIn
 from core.controller import BaseController
+from core.exceptions import DuplicateValueException
 
 
 class CategoryController(BaseController[DBCategory]):
@@ -9,3 +11,13 @@ class CategoryController(BaseController[DBCategory]):
         repository: CategoryRepository,
     ) -> None:
         super().__init__(DBCategory, repository)
+        self.repository = repository
+
+    async def create(self, data: CategoryIn):
+
+        if await self.repository.get_by_name(data.name):
+            raise DuplicateValueException()
+
+        category = await self.create(**data.model_dump(exclude_none=True))
+        await self.commit()
+        return category
--- backend/app/repositories/category.py
+++ backend/app/repositories/category.py
@@ -7,3 +7,6 @@ from core.repository import BaseRepository
 class CategoryRepository(BaseRepository[DBCategory]):
     def __init__(self, session: AsyncSession) -> None:
         super().__init__(DBCategory, session)
+
+    async def get_by_name(self, name: str) -> DBCategory | None:
+        return await self.get_one_by_filters({"name": name})
