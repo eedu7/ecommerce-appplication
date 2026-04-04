cbbf9ac51bc7435769b0b2731ce435ecec83ba6d
eedu7 <mueedahmad067@gmail.com>
2026-04-04 13:25:29 +0500

    chore: made the use-category hook to accept the limit and offset parameter

--- frontend/src/features/category/hooks/use-categories.ts
+++ frontend/src/features/category/hooks/use-categories.ts
@@ -2,8 +2,14 @@ import { useQuery, UseQueryResult } from "@tanstack/react-query";
 import { apiBrowserClient } from "@/lib/api/api.client";
 import { Category } from "@/features/category/category.types";
 
-export const useCategories = (): UseQueryResult<Category[], Error> =>
+export const useCategories = ({
+    offset = 0,
+    limit = 30,
+}: {
+    offset?: number;
+    limit?: number;
+} = {}): UseQueryResult<Category[], Error> =>
     useQuery({
         queryKey: ["categories", "use-categories"],
-        queryFn: async () => await apiBrowserClient("/categories"),
+        queryFn: async () => await apiBrowserClient(`/categories/?offset=${offset}&limit=${limit}`),
     });
