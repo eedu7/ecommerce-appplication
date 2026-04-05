806d5329a7c6b17f4e7fddea91af8f0446987b89
eedu7 <mueedahmad067@gmail.com>
2026-04-05 09:12:30 +0500

    chore: formatted the code

--- frontend/src/features/product/components/product-columns.tsx
+++ frontend/src/features/product/components/product-columns.tsx
@@ -1,4 +1,17 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { Product } from "@/features/product/product.types";
 
-export const productColumns: ColumnDef<Product>[] = [];
+export const productColumns: ColumnDef<Product>[] = [
+    {
+        accessorKey: "name",
+        header: "Name",
+    },
+    {
+        accessorKey: "price",
+        header: "Price",
+    },
+    {
+        accessorKey: "sku",
+        header: "Slug",
+    },
+];
--- frontend/src/features/product/components/product-data-table.tsx
+++ frontend/src/features/product/components/product-data-table.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { DataTable } from "@/features/dashboard/components/data-table";
-import { productColumns } from "@/features/product/components/product-columns-actions";
 import { Button } from "@/components/ui/button";
 import { productSeedData } from "@/features/product/product.seed";
+import { productColumns } from "@/features/product/components/product-columns";
 
 export const ProductDataTable = () => {
     return <DataTable columns={productColumns} createData={<Button>Create Product</Button>} data={productSeedData} />;
